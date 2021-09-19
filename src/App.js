38ba854554ad49304f1df1455a5d99b5db85ff9b
showkat-ALI <cpianshowkat@gmail.com>
2021-09-19 22:24:00 +0600

    ui

--- src/App.js
+++ src/App.js
@@ -2,24 +2,65 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const intro =[
+    {name:"showkat",intarest:"web dev"},
+    {name:"Dip",intarest:"Guta guti"},
+    {name:"Alif",intarest:"Practical electronics"},
+    {name:"showab",intarest:"book selling"},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+      intro.map(person=> <Identity name = {person.name} intarest = {person.intarest}></Identity>)
+    }
+
     </div>
   );
+}
+// function Intro(props){
+//   const student ={
+//     backgroundColor:"darkblue",
+//     color: "white",
+//     padding: "20px",
+//     margin: "10px",
+//     border: "2px solid green",
+//     borderRadius:"20px"
+
+
+//   }
+//   return(
+//   <div style={{backgroundColor:"darkblue",
+//   color: "white",
+//   padding: "20px",
+//   margin: "10px",
+//   border: "2px solid green",
+//   borderRadius:"20px",
+//   width: "350px"
+// }}>
+//   <h1>{props.name}</h1>
+//   <p>Cpi </p>
+//   <p>Department: Electronics</p>
+//   <p>Interest in: {props.Interest}</p>
+//   </div>
+//   );
+
+
+// }
+function Identity(props){
+  return(
+    <div style={{backgroundColor:"darkblue",
+    color: "white",
+    padding: "20px",
+    margin: "10px",
+    border: "2px solid green",
+    borderRadius:"20px",
+    width: "350px"
+  }}>
+      <h1> name: {props.name}</h1>
+      <h2>interest: {props.intarest}</h2>
+    </div>
+
+  )
 }
 
 export default App;
